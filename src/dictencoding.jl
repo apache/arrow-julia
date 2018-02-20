b78106583749eda760c2200d164974a6582c8070
ExpandingMan <savastio@gmail.com>
2018-02-20 12:04:03 -0500

    Made DictEncoding more flexible.

--- src/dictencoding.jl
+++ src/dictencoding.jl
@@ -1,38 +1,20 @@
 
+# TODO is this really a reasonable default way of doing nulls?
+
+const ReferenceType = AbstractPrimitive{T} where T<:Union{Int32,Union{Int32,Missing}}
+
+
 """
     DictEncoding{P<:ArrowVector,J} <: ArrowVector{J}
-
-An arrow formatted dictionary encoding of a vector.  This is essentially an arrow primitive or list
-of unique values with a primitive array of `Int32` specifying the values.  The structure is similar to
-Julia `CategoricalArray`s.
-
-## Constructors
-    DictEncoding(refs::Primitive{Int32}, pool::ArrowVector)
-    DictEncoding(data::Vector{UInt8}, refs_idx::Integer, len::Integer, pool::ArrowVector)
-    DictEncoding(data::Vector{UInt8}, refs_idx::Integer, pool_idx::Integer, x::CategoricalArray)
-    DictEncoding(data::Vector{UInt8}, refs_idx::Integer, pool_bmask_idx::Integer, pool_vals_idx::Integer,
-                 x::CategoricalArray)
-    DictEncoding(data::Vector{UInt8}, i::Integer, x::CategoricalArray)
-    DictEncoding(Array, x::CategoricalArray)
-    DictEncoding(x::CategoricalArray)
-
-If `Array` is passed a contiguous array will be allocated.
-
-### Arguments
-- `refs`: a `Primitive` providing references to values in `pool`
-- `data`: a buffer for storing the data
-- `refs_idx`: the location in `data` of the references
-- `pool_idx`: the location in `data` of the value pool
-- `x`: a `CategoricalArray` to be stored as a `DictEncoding`
 """
-struct DictEncoding{J,P<:ArrowVector} <: ArrowVector{J}
-    refs::Primitive{Int32}
+struct DictEncoding{J,R<:ReferenceType,P<:ArrowVector} <: ArrowVector{J}
+    refs::R
     pool::P
 end
 export DictEncoding
 
-function DictEncoding{J}(refs::Primitive{Int32}, pool::P) where {J,P<:ArrowVector}
-    DictEncoding{J,P}(refs, pool)
+function DictEncoding{J}(refs::R, pool::P) where {J,R<:ReferenceType,P<:ArrowVector}
+    DictEncoding{J,R,P}(refs, pool)
 end
 
 function DictEncoding{J}(data::Vector{UInt8}, refs_idx::Integer, len::Integer, pool::P
@@ -40,19 +22,37 @@ function DictEncoding{J}(data::Vector{UInt8}, refs_idx::Integer, len::Integer, p
     refs = Primitive{Int32}(data, refs_idx, len)
     DictEncoding{J}(refs, pool)
 end
-
-function DictEncoding(data::Vector{UInt8}, refs_idx::Integer, pool_idx::Integer,
-                      x::CategoricalArray{T,1,U}) where {J,U,T<:Union{J,Union{J,Missing}}}
-    refs = Primitive{Int32}(data, refs_idx, getrefs(x))
-    pool = Primitive{J}(data, pool_idx, getlevels(x))
-    DictEncoding{T}(refs, pool)
+function DictEncoding{Union{J,Missing}}(data::Vector{UInt8}, refs_bmask_idx::Integer,
+                                        refs_values_idx::Integer,
+                                        len::Integer, pool::P) where {J,P<:ArrowVector}
+    refs = NullablePrimitive{Int32}(data, refs_bmask_idx, refs_values_idx, len)
+    DictEncoding{Union{J,Missing}}(refs, pool)
 end
 
-function DictEncoding(data::Vector{UInt8}, i::Integer, x::CategoricalArray{T,1,U}
-                     ) where {J,U,T<:Union{J,Union{J,Missing}}}
+function DictEncoding(data::Vector{UInt8}, refs_idx::Integer, pool_idx::Integer,
+                      x::CategoricalArray{J,1,U}) where {J,U}
+    refs = Primitive{Int32}(data, refs_idx, getrefs(x))
+    pool = Primitive{J}(data, pool_idx, getlevels(x))
+    DictEncoding{J}(refs, pool)
+end
+function DictEncoding(data::Vector{UInt8}, refs_bmask_idx::Integer, refs_values_idx::Integer,
+                      pool_idx::Integer, x::CategoricalArray{Union{J,Missing},1,U}) where {J,U}
+    refs = NullablePrimitive{Int32}(data, refs_bmask_idx, refs_values_idx, getrefs(x))
+    pool = Primitive{J}(data, pool_idx, getlevels(x))
+    DictEncoding{J}(refs, pool)
+end
+
+function DictEncoding(data::Vector{UInt8}, i::Integer, x::CategoricalArray{J,1,U}
+                     ) where {J,U}
     refs = Primitive{Int32}(data, i, getrefs(x))
-    pool = Primitive{J}(data, i+refsbytes(x), getlevels(x))
+    pool = createpool(data, i+refsbytes(x), x)
     DictEncoding{T}(refs, pool)
+end
+function DictEncoding(data::Vector{UInt8}, i::Integer, x::CategoricalArray{Union{J,Missing},1,U}
+                     ) where {J,U}
+    refs = NullablePrimitive{Int32}(data, i, getrefs(x))
+    pool = createpool(data, i+refsbytes(x), x)
+    DictEncoding{J}(refs, pool)
 end
 
 function DictEncoding(::Type{<:Array}, x::CategoricalArray)
@@ -61,7 +61,7 @@ function DictEncoding(::Type{<:Array}, x::CategoricalArray)
 end
 
 function DictEncoding(x::CategoricalArray{J,1,U}) where {J,U}
-    refs = Primitive{Int32}(getrefs(x))
+    refs = arrowformat(getrefs(x))
     pool = arrowformat(getlevels(x))
     DictEncoding{J}(refs, pool)
 end
@@ -81,40 +81,55 @@ levels(d::DictEncoding) = d.pool
 export references, levels
 
 
-# TODO mark bounds checking (also, these throw errors from d.refs)
-# note that we define all of these methods to avoid method ambiguity
-getindex(d::DictEncoding, i::Integer) = d.pool[d.refs[i]+1]
-getindex(d::DictEncoding, i::AbstractVector{<:Integer}) = d.pool[d.refs[i].+1]
-getindex(d::DictEncoding, i::AbstractVector{Bool}) = d.pool[d.refs[i].+1]
-
-
-function getindex(d::DictEncoding{Union{J,Missing}}, i::Integer)::Union{J,Missing} where J
-    r = d.refs[i] + 1
-    r == 0 ? missing : d.pool[r]
+function createpool(data::Vector{UInt8}, i::Integer, x::CategoricalArray{J,1,U}) where {J,U}
+    Primitive{J}(data, i, getlevels(x))
 end
-function getindex(d::DictEncoding{Union{J,Missing}}, idx::AbstractVector{<:Integer}) where J
-    Union{J,Missing}[getindex(d, i) for i ∈ idx]
-end
-function getindex(d::DictEncoding{Union{J,Missing}}, idx::AbstractVector{Bool}) where J
-    Union{J,Missing}[getindex(d, i) for i ∈ 1:length(d) if idx[i]]
+function createpool(data::Vector{UInt8}, i::Integer, x::CategoricalArray{T,1,U}
+                   ) where {J<:AbstractString,U,T<:Union{J,Union{J,Missing}}}
+    List{J}(data, i, getlevels(x))
 end
 
 
-nullcount(d::DictEncoding{Union{J,Missing}}) where J = sum(d.refs .< 0)
+# both defined to avoid method ambiguity
+isnull(d::DictEncoding, i::Integer) = isnull(d.refs, i)
+isnull(d::DictEncoding, idx::AbstractVector{<:Integer}) = isnull(d.refs, idx)
+
+function getindex(d::DictEncoding{J}, i::Integer)::J where J
+    isnull(d, i) ? missing : d.pool[d.refs[i]+1]
+end
+function getindex(d::DictEncoding{J}, idx::AbstractVector{<:Integer}) where J
+    J[getindex(d, i) for i ∈ idx]
+end
+function getindex(d::DictEncoding{J}, idx::AbstractVector{Bool}) where J
+    J[getindex(d, i) for i ∈ 1:length(d) if idx[i]]
+end
+
+
+nullcount(d::DictEncoding{Union{J,Missing}}) where J = nullcount(d.refs)
 
 
 #====================================================================================================
     utilities specific to DictEncoding
 ====================================================================================================#
 getrefs(x::CategoricalArray) = convert(Vector{Int32}, x.refs) .- Int32(1)
+function getrefs(x::CategoricalArray{Union{J,Missing},1,U}) where {J,U}
+    refs = Vector{Union{Int32,Missing}}(length(x))
+    for i ∈ 1:length(x)
+        x.refs[i] == 0 ? (refs[i] = missing) : (refs[i] = x.refs[i] - 1)
+    end
+    refs
+end
 
 getlevels(x::CategoricalArray) = x.pool.index
 
 refsbytes(len::Integer) = padding(sizeof(Int32)*len)
+refsbytes(::Type{Union{J,Missing}}, len::Integer) where J = bitmaskbytes(len) + refsbytes(len)
 refsbytes(x::AbstractVector) = refsbytes(length(x))
+refsbytes(::Type{Union{J,Missing}}, x::AbstractVector) where J = refsbytes(Union{J,Missing}, length(x))
+refsbytes(x::AbstractVector{Union{J,Missing}}) where J = refsbytes(Union{J,Missing}, length(x))
 
-totalbytes(x::CategoricalArray) = refsbytes(x) + totalbytes(levels(x))
+totalbytes(x::CategoricalArray) = refsbytes(x) + totalbytes(getlevels(x))
 function totalbytes(x::CategoricalArray{Union{J,Missing},1,U}) where {J,U}
-    refsbytes(x) + totalbytes(Union{J,Missing}, levels(x))
+    refsbytes(Union{J,Missing}, x) + totalbytes(getlevels(x))
 end
 
