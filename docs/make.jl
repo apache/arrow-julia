b45eb17dfe404fbb8cbe8d7ac003f5ed52c18c45
Jacob Quinn <quinn.jacobd@gmail.com>
2020-10-28 00:16:34 -0600

    docs infra

--- docs/make.jl
+++ docs/make.jl
@@ -11,9 +11,4 @@ makedocs(
     ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(repo = "github.com/JuliaData/Arrow.jl.git")
