84b959f4abf37b2b2d5ede14b1aab9102db6f39d
Sutou Kouhei <kou@clear-code.com>
2022-02-23 08:39:22 +0900

    Introduce Release audit tool (Rat) (#289)
    fix #286
    
    * dev/release/: Add scripts for Rat related
    * *: Add missing license header
    * scripts/update_apache_arrow_code.jl: Remove because it's no longer needed
    
    Add a CI job that audits licenses.

---
+++ dev/release/run_rat.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+set -eu
+
+RELEASE_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
+
+RAT_VERSION=0.13
+
+RAT_JAR="${RELEASE_DIR}/apache-rat-${RAT_VERSION}.jar"
+if [ ! -f "${RAT_JAR}" ]; then
+  curl \
+    --fail \
+    --output "${RAT_JAR}" \
+    --silent \
+    https://repo1.maven.org/maven2/org/apache/rat/apache-rat/${RAT_VERSION}/apache-rat-${RAT_VERSION}.jar
+fi
+
+RAT="java -jar ${RAT_JAR} -x "
+RAT_XML="${RELEASE_DIR}/rat.xml"
+$RAT $1 > "${RAT_XML}"
+FILTERED_RAT_TXT="${RELEASE_DIR}/filtered_rat.txt"
+if ${PYTHON:-python3} \
+     "${RELEASE_DIR}/check_rat_report.py" \
+     "${RELEASE_DIR}/rat_exclude_files.txt" \
+     "${RAT_XML}" > \
+     "${FILTERED_RAT_TXT}"; then
+  echo "No unapproved licenses"
+else
+  cat "${FILTERED_RAT_TXT}"
+  N_UNAPPROVED=$(grep "NOT APPROVED" "${FILTERED_RAT_TXT}" | wc -l)
+  echo "${N_UNAPPROVED} unapproved licenses. Check Rat report: ${RAT_XML}"
+  exit 1
+fi
