3c850c5048c59d51f2311ad5b97ff3f4521ee102
Joao Aparicio <jpmn.aparicio@gmail.com>
2024-01-30 10:28:09 -0600

    Update src/arraytypes/list.jl
    Co-authored-by: Ben Baumgold <4933671+baumgold@users.noreply.github.com>

--- src/arraytypes/list.jl
+++ src/arraytypes/list.jl
@@ -131,7 +131,7 @@ end
 
 Base.IndexStyle(::Type{<:ToList}) = Base.IndexLinear()
 Base.size(x::ToList{T,S,A,I}) where {T,S,A,I} =
-    (length(x.inds) == 0 ? zero(I) : x.inds[end],)
+    (isempty(x.inds) ? zero(I) : x.inds[end],)
 
 function Base.pointer(A::ToList{UInt8}, i::Integer)
     chunk = searchsortedfirst(A.inds, i)
